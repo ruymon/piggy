3e1d0a0c5a8c7ce108a2f3f3b2eab21ba56e7307
ruymon <ruystfrancis@gmail.com>
2021-07-12 18:52:14 -0300

    Update SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,29 +7,29 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5" />
+        <meta http-equiv="Content-Language" content="pt-br"/>
+        <meta name='application-name' content='Piggy' />
+        <meta name="description" content="Piggy é a ferramenta de código aberto mais fácil de organizar e gerenciar suas finanças enquanto aprende! Seu porquinho digital." />
+        <meta name="keywords" content="piggy porquinho hub Piggy financas tracker expense dinheiro ruymon" />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='Piggy' />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name="theme-color" content="#4CA145" />
+
+
+
+
+        <title>Piggy</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
+
+        <link href="/icons/icon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/icon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
-        <meta name="theme-color" content="#317EFB" />
+
       </Head>
       <Component {...pageProps} />
     </>
