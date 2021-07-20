e4f685fc4b6f4cc1dca0ea0ef704c2ce15e6a79b
ruymon <ruystfrancis@gmail.com>
2021-07-20 12:35:38 -0300

    Create Global Style

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F2F5;
+    color: #363F5F;
+  }
+`;
