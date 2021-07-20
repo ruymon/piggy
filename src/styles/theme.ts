f70cf7ffd0562a4feffb9454d8d30a8a190f0f28
ruymon <ruystfrancis@gmail.com>
2021-07-20 12:47:33 -0300

    Create Default Theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    background: '#F0F2F5',
+    title: '#363F5F',
+    text: '#969CB2',
+    primary: '#4CA145',
+    secondary: '#68BA61',
+    lightGreen: '#72C845',
+    danger: '#E52E4D',
+    shade: '#FFFFFF'
+  }
+};
+
+export default theme;
