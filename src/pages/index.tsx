0377c2a086854ebe33f6c10a3c2464e54a5f2aa5
ruymon <ruystfrancis@gmail.com>
2021-07-20 11:16:24 -0300

    Install Notification Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,4 @@
-import React from "react";
-
 export default function Home() {
-
-  React.useEffect(() => {
-    function showInstallPromotion() {
-      alert("Install APP");
-    };
-
-    // Initialize deferredPrompt for use later to show browser install prompt.
-    let deferredPrompt;
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    showInstallPromotion();
-    // Optionally, send analytics event that PWA install promo was shown.
-    console.log(`'beforeinstallprompt' event was fired.`);
-    });
-
-  }, []);
-
   return (
     <h1>Piggy</h1>
   )
