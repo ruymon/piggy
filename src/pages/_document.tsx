9d693f58f640a9bd811f2cb0f6d30fcb8abfde75
ruymon <ruystfrancis@gmail.com>
2021-07-20 12:10:55 -0300

    Fix _document types errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
-import Document from 'next/document'
+import React from 'react';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
